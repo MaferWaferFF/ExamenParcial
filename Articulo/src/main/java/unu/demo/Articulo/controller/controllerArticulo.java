64941769b3ed0f7c39c369c557afd9051cd52c0f
MaferWaferFF <marifegrandezunu@gmail.com>
2025-11-11 17:52:25 -0500

    segundo avance

---
+++ Articulo/src/main/java/unu/demo/Articulo/controller/controllerArticulo.java
@@ -0,0 +1,38 @@
+package unu.demo.Articulo.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import unu.demo.Articulo.model.modelArticulo;
+import unu.demo.Articulo.repository.repositoryArticulo;
+import unu.demo.Articulo.services.serviceArticulo;
+
+@RestController
+@RequestMapping("/api/articulos")
+
+public class controllerArticulo {
+     private static final Logger logger = LoggerFactory.getLogger(controllerArticulo.class);
+
+    @Autowired
+    private repositoryArticulo artiRepository;
+   //private serviceArticulo ariService;
+
+    @PostMapping
+    public modelArticulo crearAlumno(@RequestBody modelArticulo articulo) {
+        return artiRepository.save(articulo);
+    }
+
+    @GetMapping
+    public List<modelArticulo> obtenerAlumnos() {
+        return artiRepository.findAll();
+    }
+
+}
--- Articulo/src/main/java/unu/demo/Articulo/model/modelArticulo.java
+++ Articulo/src/main/java/unu/demo/Articulo/model/modelArticulo.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table (name = "Articulo")
+@Table (name = "pago")
 public class modelArticulo {
     private static final long serialVersionUID = 1L;
 
